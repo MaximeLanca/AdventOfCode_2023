8b6b5c64782e5622f014adb73ed3b5366a18f182
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-10-15 16:47:39 +0200

    renamed variables

--- DayOne_01/main.py
+++ DayOne_01/main.py
@@ -1,8 +1,8 @@
 def calculate_total_numbers() -> list:
     with open('input.txt', 'r') as file:
-        lines_list = [lines.strip() for lines in file]
+        data_list = [lines.strip() for lines in file]
 
-    return lines_list
+    return data_list
 
 
 def find_numbers_in_list():
@@ -14,15 +14,15 @@ def find_numbers_in_list():
         digits = [char for char in line if char.isdigit()]
         digits_list_by_line.append(digits)
 
-    for numbers_list in digits_list_by_line:
-        if len(numbers_list) == 1:
-            digit = numbers_list[0]
+    for digits_list in digits_list_by_line:
+        if len(digits_list) == 1:
+            digit = digits_list[0]
             number = int(digit + digit)
             total_sum_of_digits = total_sum_of_digits + number
 
         else:
-            first_digit = numbers_list[0]
-            last_digit = numbers_list[-1]
+            first_digit = digits_list[0]
+            last_digit = digits_list[-1]
             number = int(first_digit + last_digit)
             total_sum_of_digits = total_sum_of_digits + number
 
