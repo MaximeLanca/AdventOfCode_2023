00a9d77bcca2c0affd8554202df30b236dc805b9
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-10-15 13:47:19 +0200

    Algo finished

--- DayOne_01/main.py
+++ DayOne_01/main.py
@@ -2,8 +2,31 @@ def calculate_total_numbers() -> list:
     with open('input.txt', 'r') as file:
         lines_list = [lines.strip() for lines in file]
 
-    print(lines_list)
     return lines_list
 
 
-calculate_total_numbers()
+def find_numbers_in_list():
+    list_for_analysis = calculate_total_numbers()
+    digits_list_by_line = []
+    total_sum_of_digits = 0
+
+    for line in list_for_analysis:
+        digits = [char for char in line if char.isdigit()]
+        digits_list_by_line.append(digits)
+
+    for numbers_list in digits_list_by_line:
+        if len(numbers_list) == 1:
+            digit = numbers_list[0]
+            number = int(digit + digit)
+            total_sum_of_digits = total_sum_of_digits + number
+
+        else:
+            first_digit = numbers_list[0]
+            last_digit = numbers_list[-1]
+            number = int(first_digit + last_digit)
+            total_sum_of_digits = total_sum_of_digits + number
+
+    print(total_sum_of_digits)
+
+
+find_numbers_in_list()
