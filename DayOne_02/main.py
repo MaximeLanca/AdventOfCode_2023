fa883fd28f3685441d7898356b68af5e34adb9ee
MaximeLanca <lanca.maxime.ber@gmail.com>
2024-10-16 22:35:02 +0200

    Algo in progress

--- DayOne_02/main.py
+++ DayOne_02/main.py
@@ -13,27 +13,27 @@ def find_words_in_data_list():
     list_for_analysis = get_alphanumeric_data()
     dict_ref = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
     # line = tsgbzmgbonethreedrqzbhxjkvcnm3
+    #for line in list_for_analysis:
+    #    # decomposed_line = ["tsgbzmgbonethreedrqzbhxjkvcnm","3"]
+    #    decomposed_line = re.findall(r'\d+|[a-zA-Z]+', line)
+    #    for key in dict_ref.keys():
+    #        provisional_list = []
+    #        # data = tsgbzmgbonethreedrqzbhxjkvcnm
+    #        for data in decomposed_line:
+    #            if key in data:
+    #                provisional_list.append(key)
+    #                index = decomposed_line.index(data)
+    #                decomposed_line[index] = provisional_list
+    #
     for line in list_for_analysis:
-        # decomposed_line = ["tsgbzmgbonethreedrqzbhxjkvcnm","3"]
-        decomposed_line = re.findall(r'\d+|[a-zA-Z]+', line)
-        for key in dict_ref.keys():
-            provisional_list = []
-            # data = tsgbzmgbonethreedrqzbhxjkvcnm
-            for data in decomposed_line:
-                if key in data:
-                    provisional_list.append(key)
-                    index = decomposed_line.index(data)
-                    decomposed_line[index] = provisional_list
+            decomposed_line = re.findall(r'\d+|[a-zA-Z]+', line)
+            decomposed_line = [key if key in data else data for data in decomposed_line for key in dict_ref]
 
-        for line in list_for_analysis:
-            # Séparer chiffres et lettres
-            decomposed_line = re.findall(r'\d+|[a-zA-Z]+', line)            #total_sum_of_digits += dict_ref[key]
-                    #print(total_sum_of_digits)
-            # Remplacer les parties texte par les clés du dictionnaire
-            decomposed_line = [print(decomposed_line)
-                key if key in data else data for data in decomposed_line for key in dict_ref]
+    print(decomposed_line)
 
-            print(decomposed_line)
+
+
+
 find_words_in_data_list()
 #
 # for line in list_for_analysis:
